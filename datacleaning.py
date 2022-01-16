10fddc207511a64d52b38de14b2e37c210535e27
ATHARVAMISHRA22092008 <56999105+ATHARVAMISHRA22092008@users.noreply.github.com>
2022-01-16 08:56:34 +0800

    Add files via upload

---
+++ datacleaning.py
@@ -0,0 +1,53 @@
+import csv
+import pandas as pd
+df=pd.read_csv('finalhope.csv')
+
+del df["hyperlink"]
+
+del df["pl_name"]
+del df["pl_orbper"]
+del df["pl_orbpererr1"]
+del df["pl_orbpererr2"]
+del df["pl_orbperlim"]
+del df["pl_orbsmax"]
+del df["pl_orbsmaxerr1"]
+del df["pl_orbsmaxerr2"]
+del df["pl_orbsmaxlim"]
+del df["pl_orbeccen"]
+del df["pl_orbeccenerr1"]
+del df["pl_orbeccenerr2"]
+del df["pl_orbeccenlim"]
+del df["pl_bmassj"]
+del df["pl_bmassjerr1"]
+del df["pl_bmassjerr2"]
+del df["pl_bmassjlim"]
+del df["pl_bmassprov"]
+del df["pl_radj"]
+del df["pl_radjerr1"]
+del df["pl_radjerr2"]
+del df["pl_radjlim"]
+del df["ra"]
+del df["dec"]
+del df["st_tefferr1"]
+del df["st_tefferr2"]
+del df["st_tefflim"]
+del df["st_masserr1"]
+del df["st_masserr2"]
+del df["st_masslim"]
+del df["st_raderr1"]
+del df["st_raderr2"]
+del df["st_radlim"]
+
+df = df.rename({
+                'pl_hostname': "solar_system_name", 
+                'pl_discmethod': "planet_discovery_method", 
+                'pl_orbincl': "planet_orbital_inclination", 
+                'pl_dens': "planet_density", 
+                'ra_str': "right_ascension", 
+                'dec_str': "declination", 
+                "st_teff": "host_temperature", 
+                'st_mass': "host_mass", 
+                'st_rad': "host_radius"
+            }, axis='columns')
+
+df.to_csv('main.csv') 
